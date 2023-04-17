55352dc1534930fbac48d532ceb342a31c6a7184
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-04-17 23:28:34 +0900

    databinding

--- app/src/main/java/com/api/chatgpt/MainActivity.kt
+++ app/src/main/java/com/api/chatgpt/MainActivity.kt
@@ -2,10 +2,12 @@ package com.api.chatgpt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { DataBindingUtil.bind(this,R.layout.activity_main) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
