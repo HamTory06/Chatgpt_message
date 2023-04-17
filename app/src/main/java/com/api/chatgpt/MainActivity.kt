d0d1da58313278c0e1fcb9f8010fcf00f3de30ba
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-04-17 23:32:34 +0900

    databinding 애러 해결

--- app/src/main/java/com/api/chatgpt/MainActivity.kt
+++ app/src/main/java/com/api/chatgpt/MainActivity.kt
@@ -3,11 +3,12 @@ package com.api.chatgpt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.api.chatgpt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy { DataBindingUtil.bind(this,R.layout.activity_main) }
+    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        binding
     }
 }
